77f90b592dfc2593f390b63d1e491372e49e4d22
juevn <127848766+juevn@users.noreply.github.com>
2026-04-14 11:52:50 +0000

    Deploying to gh-pages from @ juevn/juevn.github.io@d467a27dfddbceaede1de1df37dea4d4bd42f870 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -100,21 +100,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%65%6B%69%6D@%64%62%6C%61%62.%70%6F%73%74%65%63%68.%61%63.%6B%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/juevn", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/jueun-kim-510377232", "_blank");
         },
       },{
         id: 'social-scholar',
@@ -122,13 +122,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
